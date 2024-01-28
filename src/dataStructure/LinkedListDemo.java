df530c29f2047a89cf82dc2f6be9a0f4dc0adfbc
Oasis <gongstats@gmail.com>
2024-01-28 17:30:26 +0900

    add "LinkedListDemo"

--- src/dataStructure/LinkedListDemo.java
+++ src/dataStructure/LinkedListDemo.java
@@ -1,29 +1,38 @@
 package dataStructure;
-import java.util.Queue;
+
 import java.util.LinkedList;
 
 public class LinkedListDemo {
 
 	public static void main(String[] args) {
-		// FIFO = First In First Out data structure
-		// a collection for holding elements prior to processing the liner structure.
-		
-		// add = enqueue, offer()
-		// remove = dequeue, poll()
-		
-		Queue<String> q = new LinkedList<String>();
-		
-		q.offer("Anna");
-		q.offer("Bob");
-		q.offer("Christina");
-		q.offer("Artmio");
+		// TODO Auto-generated method stub
 
-		System.out.println(q.isEmpty());
-		System.out.println(q.size());
-		System.out.println(q.contains("Anna"));
-		System.out.println(q);
-		System.out.println(q.poll());
-		System.out.println(q.peek());
+		LinkedList<String> StackLikelinkedList = new LinkedList<String>();
+
+		// Take linkedList as a Stack
+		StackLikelinkedList.push("A");
+		StackLikelinkedList.push("B");
+		StackLikelinkedList.push("C");
+		StackLikelinkedList.push("D");
+
+		StackLikelinkedList.pop();
+
+		System.out.println(StackLikelinkedList);
+
+		// Take linkedList as a Queue
+		LinkedList<String> QueueLikeLinkedList = new LinkedList<String>();
+
+		QueueLikeLinkedList.offer("Anna");
+		QueueLikeLinkedList.offer("Christina");
+		QueueLikeLinkedList.offer("Natsuko");
+		QueueLikeLinkedList.offer("Maki");
+
+		QueueLikeLinkedList.add(1, "Artmio");
+		QueueLikeLinkedList.addFirst("Hatsune");
+		String first = QueueLikeLinkedList.removeFirst();
+		System.out.println(first);
+		System.out.println(QueueLikeLinkedList);
+
 	}
 
 }
